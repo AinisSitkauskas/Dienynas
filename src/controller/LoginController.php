a2d8ad666d240082f47dc6ed8c16e7ade1ca4005
Ainis Sitkauskas <ainis795@gmail.com>
2019-04-09 19:09:20 +0300

    login

--- src/controller/LoginController.php
+++ src/controller/LoginController.php
@@ -12,29 +12,28 @@ class LoginController {
 
     public function loginAction() {
 
-        if (empty($_POST['userName']) && empty($_POST['password'])) {
+        if (empty($_POST['userName']) || empty($_POST['password'])) {
             include("views/login.php");
-        } else {
-            $userName = $_POST['userName'];
-            $password = $_POST['password'];
-            $status = $this->userExist($userName, $password);
-
-            if ($status == FALSE) {
-                include("views/error.php");
-            } elseif ($status == TRUE) {
-                setcookie("login", 'login', time() + self::COOKIE_EXPIRE_TIME, "/");
-                $_GET['controller'] = "welcome";
-                $_GET['action'] = "welcome";
-                header("Location: index.php");
-            }
+            return;
         }
+
+        $userName = $_POST['userName'];
+        $password = $_POST['password'];
+
+        if ($this->userExist($userName, $password) == FALSE) {
+            include("views/error.php");
+            return;
+        }
+
+        setcookie("login", 'login', time() + self::COOKIE_EXPIRE_TIME, "/");
+        $_GET['controller'] = "welcome";
+        $_GET['action'] = "welcome";
+        header("Location: index.php");
     }
 
     public function logoutAction() {
         if (isset($_POST['logout'])) {
             setcookie("login", 'logout', time() - self::COOKIE_EXPIRE_TIME, "/");
-            $_GET['controller'] = "login";
-            $_GET['action'] = "login";
             header("Location: index.php");
         }
     }
@@ -43,9 +42,7 @@ class LoginController {
         $sqlQuery = "SELECT * FROM users WHERE userName='$userName' AND password='$password'";
         $result = $this->connection->query($sqlQuery);
         if ($result->num_rows > 0) {
-            return $status = TRUE;
-        } else {
-            return $status = FALSE;
+            return TRUE;
         }
     }
 
